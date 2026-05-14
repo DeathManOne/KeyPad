feb267d5cfdc2ab98d041130484aeb262653a502
DeathManOne <37964659+DeathManOne@users.noreply.github.com>
2026-05-14 20:00:03 +0200

    Add files via upload

--- include/keypad.h
+++ include/keypad.h
@@ -1,3 +1,26 @@
+/*
+ * keypad.cpp
+ *
+ * Copyright (c) 2026 DeathManOne
+ * https://github.com/DeathManOne
+ * 
+ * This file is part of the KeyPad library.
+ *
+ * KeyPad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KeyPad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KeyPad.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef _KEYPAD_H_
 #define _KEYPAD_H_
 
@@ -6,23 +29,43 @@
 
 class KeyPad {
     private:
-        const char KEYPAD[4][4] = {
+        static constexpr char KEYPAD_4X4[4][4] = {
             {'1','2','3','A'},
             {'4','5','6','B'},
             {'7','8','9','C'},
             {'*','0','#','D'}
         };
+        static constexpr char KEYPAD_4X3[4][3] = {
+            {'1','2','3'},
+            {'4','5','6'},
+            {'7','8','9'},
+            {'*','0','#'}
+        };
+
         std::vector<int> *_PINS_COLUMNS, *_PINS_ROWS;
+        bool *_VALID;
         int _getRow() const;
         int _getColumn(int rowValue) const;
+        int _getKeyValue(int rowValue, int columnValue) const;
         void _clear() const;
+
     public:
         /**
          * @brief constructor
-         * @param pinsColumns vector (ex: {4,9,10,14})
-         * @param pinsRows vector (ex: {7,6,8,3})
+         * @param pinsColumns GPIO columns pins
+         * @param pinsRows GPIO rows pins
          */
         KeyPad(std::vector<int> pinsColumns, std::vector<int> pinsRows);
+
+        /**
+         * @brief disable copy constructor
+         */
+        KeyPad(const KeyPad&) = delete;
+
+        /**
+         * @brief disable copy assignment
+         */
+        KeyPad& operator=(const KeyPad&) = delete;
 
         /**
          * @brief destructor
@@ -30,10 +73,17 @@ class KeyPad {
         ~KeyPad();
 
         /**
-         * @brief get the pushed key
-         * @param waitTimeout (default: 10) max wait time to push in seconds
-         * @return char of number/letter you have pushed or 0 (false) in case of timeout
+         * @brief check if keypad configuration is valid
+         * @return true if keypad is valid, otherwise false
          */
-        char getKey(int waitTimeout = 10) const;
+        inline bool isValid() const { return *this->_VALID; }
+
+        /**
+         * @brief wait for key press
+         * @param waitTimeoutSeconds (default: 10) timeout in seconds
+         * @return pressed key or -1 on timeout/error
+         */
+        int getKey(uint32_t waitTimeoutSeconds = 10) const;
 };
+
 #endif
--- src/keypad.cpp
+++ src/keypad.cpp
@@ -1,8 +1,32 @@
+/*
+ * keypad.cpp
+ *
+ * Copyright (c) 2026 DeathManOne
+ * https://github.com/DeathManOne
+ * 
+ * This file is part of the KeyPad library.
+ *
+ * KeyPad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KeyPad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KeyPad.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "../include/keypad.h"
 
 KeyPad::KeyPad(std::vector<int> pinsColumns, std::vector<int> pinsRows) {
     this->_PINS_COLUMNS = new std::vector<int>(pinsColumns);
     this->_PINS_ROWS = new std::vector<int>(pinsRows);
+    this->_VALID = new bool(this->_PINS_ROWS->size() == 4 && (this->_PINS_COLUMNS->size() == 3 || this->_PINS_COLUMNS->size() == 4));
     this->_clear();
 }
 
@@ -10,6 +34,7 @@ KeyPad::~KeyPad() {
     this->_clear();
     delete this->_PINS_COLUMNS;
     delete this->_PINS_ROWS;
+    delete this->_VALID;
 }
 
 void KeyPad::_clear() const {
@@ -27,50 +52,84 @@ int KeyPad::_getRow() const {
         digitalWrite(column, LOW);
     }
 
+    delay(1);
     int rowValue = -1;
-    for (int r = 0; r < this->_PINS_ROWS->size(); r++) {
-        if (!digitalRead(this->_PINS_ROWS->at(r)))
-            { rowValue = r; }
+
+    for (size_t r = 0; r < this->_PINS_ROWS->size(); r++) {
+        if (!digitalRead(this->_PINS_ROWS->at(r))) {
+            rowValue = static_cast<int>(r);
+            break;
+        }
     }
-    return (rowValue < 0 || rowValue > 3) ? -1 : rowValue;
+    return rowValue;
 }
 
 int KeyPad::_getColumn(int rowValue) const {
+    if (rowValue < 0 || rowValue >= static_cast<int>(this->_PINS_ROWS->size()))
+        { return -1; }
     for (int column : *this->_PINS_COLUMNS)
         { pinMode(column, INPUT_PULLDOWN); }
     pinMode(this->_PINS_ROWS->at(rowValue), OUTPUT);
     digitalWrite(this->_PINS_ROWS->at(rowValue), HIGH);
 
+    delay(1);
     int columnValue = -1;
-    for (int c = 0; c < this->_PINS_COLUMNS->size(); c++) {
-        if (digitalRead(this->_PINS_COLUMNS->at(c)))
-            { columnValue = c; }
+
+    for (size_t c = 0; c < this->_PINS_COLUMNS->size(); c++) {
+        if (digitalRead(this->_PINS_COLUMNS->at(c))) {
+            columnValue = static_cast<int>(c);
+            break;
+        }
     }
-    return (columnValue < 0 || columnValue > 3) ? -1 : columnValue;
+    return columnValue;
 }
 
-char KeyPad::getKey(int waitTimeout) const {
+int KeyPad::_getKeyValue(int rowValue, int columnValue) const {
+    if (rowValue < 0 || rowValue >= 4)
+        { return -1; }
+    if (columnValue < 0 || columnValue >= static_cast<int>(this->_PINS_COLUMNS->size()))
+        { return -1; }
+    if (this->_PINS_COLUMNS->size() == 4)
+        { return KeyPad::KEYPAD_4X4[rowValue][columnValue]; }
+    return KeyPad::KEYPAD_4X3[rowValue][columnValue];
+}
+
+int KeyPad::getKey(uint32_t waitTimeoutSeconds) const {
+    if (!this->isValid())
+        { return -1; }
     this->_clear();
+
     int rowValue = -1;
     int columnValue = -1;
 
-    const uint32_t timeout = millis() + (1000 * waitTimeout);
-    do {
+    const uint32_t start = millis();
+    const uint32_t timeoutMs = waitTimeoutSeconds * 1000UL;
+
+    while ((millis() - start) < timeoutMs) {
         rowValue = this->_getRow();
-        delay(50);
-    } while (rowValue == -1 && static_cast<uint32_t>(millis()) < timeout);
-    if (rowValue == -1) {
-        this->_clear();
-        return 0;
+        if (rowValue != -1)
+            { break; }
+        delay(20);
     }
 
-    do {
-        columnValue = this->_getColumn(rowValue);
-        delay(50);
-    } while (columnValue == -1 && static_cast<uint32_t>(millis()) < timeout);
+    if (rowValue == -1) {
+        this->_clear();
+        return -1;
+    }
+    delay(30);
 
+    while ((millis() - start) < timeoutMs) {
+        columnValue = this->_getColumn(rowValue);
+        if (columnValue != -1)
+            { break; }
+        delay(20);
+    }
+
+    while (this->_getRow() != -1)
+        { delay(10); }
     this->_clear();
-    if (rowValue == -1 || columnValue == -1)
-        { return 0; }
-    return KeyPad::KEYPAD[rowValue][columnValue];
+
+    if (columnValue == -1)
+        { return -1; }
+    return this->_getKeyValue(rowValue, columnValue);
 }
